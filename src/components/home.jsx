231c32339896e139426c88a981a3dac452ddd988
HE1MD4L <jeffdaren.rebote@hcdc.edu.ph>
2025-11-22 15:52:20 +0800

    second commitment

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "./home.css"; // <-- import new stylesheet
+
 
 function uid() {
   return Date.now().toString(36) + Math.random().toString(36).slice(2, 8);
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
-import "../styles/globals.css";
+import '../styles/globals.css';
+import '../styles/home.css';  // <-- add your home styles here
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
